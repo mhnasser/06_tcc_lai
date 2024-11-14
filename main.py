ed14480e12c9be682024d9503a01a5f8f69fe3de
mhnasser <mohamad.nasser@outlook.com.br>
2024-11-13 22:46:26 -0300

    adaptação para deploy no heroku

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,5 @@ def pag_ref():
 
 # Colocar o site no ar
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT"), "5000")
+    app.run(host="0.0.0.0", port=port)
